a3147bbce9c7a9ed3ee2fb05eeccf563db259617
sepharg <danielperez.arg@gmail.com>
2016-11-02 23:09:17 +0000

    added pause menu

---
+++ Mario/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+	public string levelSelect;
+	public string mainMenu;
+
+	public bool isPaused;
+	public GameObject pauseMenuCanvas;
+		
+	// Update is called once per frame
+	void Update () 
+	{
+		pauseMenuCanvas.SetActive(isPaused); // if game is paused, show greyed out menu.
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			isPaused = !isPaused;
+		}
+
+		Time.timeScale = isPaused ? 0f : 1f;
+	}
+
+	public void Resume()
+	{
+		isPaused = false;
+	}
+
+	public void LevelSelect()
+	{
+		SceneManager.LoadScene (levelSelect);
+	}
+
+	public void Quit()
+	{
+		SceneManager.LoadScene (mainMenu);
+	}
+}
