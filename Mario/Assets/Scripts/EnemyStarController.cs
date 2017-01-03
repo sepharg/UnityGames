a19cc0badfd66cbd8ccbf6ece3dbdd380fb8ca7e
sepharg <danielperez.arg@gmail.com>
2017-01-03 16:54:41 +0000

    - added enemy that shoots at player

---
+++ Mario/Assets/Scripts/EnemyStarController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets._2D;
+
+public class EnemyStarController : MonoBehaviour {
+
+    public float speed;
+    public float rotationSpeed;
+    public GameObject impactEffect;
+    public int damageToGive = 1;
+
+    private Rigidbody2D myRigidBody2D;
+    private PlatformerCharacter2D player;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<PlatformerCharacter2D>();
+
+        if (player.transform.position.x < transform.position.x) // if player is to the left of where the star is
+        {
+            speed = -speed;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidBody2D.velocity = new Vector2(speed, myRigidBody2D.velocity.y);
+        myRigidBody2D.angularVelocity = rotationSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            HealthManager.HurtPlayer(damageToGive);
+        }
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        DestroyObject(gameObject);
+    }
+}
---
+++ Mario/Assets/Scripts/ShootPlayerInRange.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets._2D;
+
+public class ShootPlayerInRange : MonoBehaviour
+{
+
+    public float playerRange;
+    public GameObject enemyStar;
+    private PlatformerCharacter2D player;
+    public Transform launchPoint;
+    public float waitBetweenShots;
+    private float shotCounter;
+
+    // Use this for initialization
+    void Start ()
+    {
+        player = FindObjectOfType<PlatformerCharacter2D>();
+        shotCounter = waitBetweenShots;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.DrawLine(new Vector3(transform.position.x - playerRange, transform.position.y, transform.position.z), new Vector3(transform.position.x + playerRange, transform.position.y, transform.position.z));
+
+	    shotCounter -= Time.deltaTime;
+
+	    if (shotCounter < 0)
+	    {
+            // moving right & player on that side & player within range
+            if (transform.localScale.x < 0 && player.transform.position.x > transform.position.x && player.transform.position.x < transform.position.x + playerRange)
+            {
+                Instantiate(enemyStar, launchPoint.position, launchPoint.rotation);
+            }
+
+            if (transform.localScale.x > 0 && player.transform.position.x < transform.position.x && player.transform.position.x > transform.position.x - playerRange)
+            {
+                Instantiate(enemyStar, launchPoint.position, launchPoint.rotation);
+            }
+            shotCounter = waitBetweenShots;
+        }       
+    }
+}
